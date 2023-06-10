3e9c142ef202d17aa5ee8a0f78de63b16b8651d8
Harsh Gupta <harshgupta102001@gmail.com>
2023-06-10 15:31:56 +0530

    added deleteFirst,deleteLast and size function

--- ll.java
+++ ll.java
@@ -1,5 +1,11 @@
 public class ll {
     Node head;
+    private int size;
+
+    ll(){
+        size=0;
+    }
+
     class Node{
         
         int data;
@@ -8,16 +14,16 @@ public class ll {
          Node(int data){
             this.data = data;
             this.next=null;
-
+            size++;
          }
     }
-    
+    //addfirst
     public void addFirst(int data){
         Node newnode = new Node(data);
         newnode.next= head;
         head= newnode;
     }
-
+    //addlast
     public void addLast(int data){
         Node newnode = new Node(data);
         if(head==null){
@@ -31,13 +37,52 @@ public class ll {
         }
         lastnode.next=newnode;
     }
+    //deletefirst
+    public void deleteFirst(){
+        if(head.next==null){
+            return;
+        }
+        head=head.next;
+        size--;
+    }
+    //deletelast
+
+    public void deleteLast(){
+        if(head==null)return;
+
+        size--;
+
+        if(head.next==null){
+            head=null;
+            return;
+        }
+
+        Node secondlast = head;
+        Node last = head.next;
+
+        while(last.next!=null){
+            secondlast= secondlast.next;
+            last = last.next;
+        }
+        secondlast.next=null;
+    }
+    //size
+    
+    public int getSize(){
+        return size;
+    }
 
     public void printNode(){
         Node currNode=head;
+        
         while(currNode!=null){
             System.out.print(currNode.data+"->");
             currNode=currNode.next;
         }
+
+        
+            System.out.print("NULL");
+        
     }
 
     public static void main(String args[]){
@@ -49,6 +94,10 @@ public class ll {
         list.addLast(5);
         list.addLast(4);
 
+        // list.deleteFirst();
+        list.deleteLast();
+
         list.printNode();
+        System.out.println("\n"+list.getSize());
     }
 }
