873f9cdab2bf1fb518998f5550510bf09d8e1eaf
Harsh Gupta <harshgupta102001@gmail.com>
2023-11-30 22:15:23 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (26.73%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int ans = 0, f = 0;
+        
+        for (int i = 31; i >= 0; i--) {
+            if (((n >> i) & 1) == 1) {
+                if (f == 0) {
+                    ans = ans + ((1 << (i + 1))) - 1;
+                    f = 1;
+                } else {
+                    ans = ans - ((1 << (i + 1)) - 1);
+                    f = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
