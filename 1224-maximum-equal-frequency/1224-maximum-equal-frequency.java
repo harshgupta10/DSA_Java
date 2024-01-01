5d15f6d051e252a25f10e1f89feffbe2fb1defaf
Harsh Gupta <harshgupta102001@gmail.com>
2024-01-01 14:02:01 +0530

    Time: 11 ms (81.97%), Space: 55.5 MB (24.59%) - LeetHub

---
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.java
@@ -0,0 +1,19 @@
+class Solution {
+     public int maxEqualFreq(int[] A) {
+        int[] count = new int[100001], freq = new int[100001];
+        int res = 0, N = A.length, a,c,d;
+        for (int n = 1; n <= N; ++n) {
+            a = A[n - 1];
+            --freq[count[a]];
+            c = ++count[a];
+            ++freq[count[a]];
+
+            if (freq[c] * c == n && n < N)
+                res = n + 1;
+            d = n - freq[c] * c;
+            if ((d == c + 1 || d == 1) && freq[d] == 1)
+                res = n;
+        }
+        return res;
+    }
+}
