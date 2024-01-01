ebb69a195b0fffd87b1bfd77fd679a520b8f5d6e
Harsh Gupta <harshgupta102001@gmail.com>
2024-01-01 13:59:20 +0530

    Time: 130 ms (5.88%), Space: 82.4 MB (23.53%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxBuilding(int n, int[][] restrictions) {
+        // sorting restrictions according to index.
+        Arrays.sort(restrictions, (a, b) -> a[0] - b[0]);
+        int len = restrictions.length + 2;
+        int[][] arr = new int[len][2];
+        arr[0][0] = 1;
+        arr[0][1] = 0;
+        arr[len-1][0] = n;
+        arr[len-1][1] = n-1;
+        for(int i = 1; i<len-1; ++i){
+            arr[i] = restrictions[i-1];
+        }
+        
+       
+        for(int i = 0; i<len-1; ++i){
+            arr[i+1][1] = Math.min(arr[i+1][1], arr[i][1] + (arr[i+1][0] - arr[i][0]));
+        }
+        
+        
+        for(int i = len-1; i>0; --i){
+            arr[i-1][1] = Math.min(arr[i-1][1], arr[i][1] + (arr[i][0] - arr[i-1][0]));
+        }
+        
+        int max = 0;
+        
+        for(int i = 0; i<len-1; ++i){
+            int j = arr[i][0], h1 = arr[i][1], k = arr[i+1][0], h2 = arr[i+1][1];
+            
+
+            int diff = Math.max(h1, h2) - Math.min(h1, h2);
+            j += diff;
+            
+            
+            max = Math.max(max, Math.max(h1, h2) + (k - j)/2);
+        }
+        
+        return max;
+    }
+}
